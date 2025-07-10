73254a92a8150a4851da202935583e6f95f55854
behai-nguyen <behai_nguyen@hotmail.com>
2025-07-10 13:02:32 +1000

    Representing Code
    Expression and Statement. Generated by .\tool\generate_ast\.

---
+++ src/expr.rs
@@ -0,0 +1,376 @@
+//> Appendix II expr
+use super::token::{LiteralValue, Token};
+
+//> expr-assign
+#[derive(Clone)]
+pub struct Assign {
+    name: Token,
+    value: Box<Expr>,
+}
+
+impl Assign {
+    pub fn new(name: Token, 
+        value: Box<Expr>
+    ) -> Self {
+        Assign {
+            name,
+            value,
+        }
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+    pub fn get_value(&self) -> &Box<Expr> {
+        &self.value
+    }
+
+}
+
+//> expr-binary
+#[derive(Clone)]
+pub struct Binary {
+    left: Box<Expr>,
+    operator: Token,
+    right: Box<Expr>,
+}
+
+impl Binary {
+    pub fn new(left: Expr, 
+        operator: Token, 
+        right: Expr
+    ) -> Self {
+        Binary {
+            left: Box::new(left),
+            operator,
+            right: Box::new(right),
+        }
+    }
+
+    pub fn get_left(&self) -> &Box<Expr> {
+        &self.left
+    }
+
+    pub fn get_operator(&self) -> &Token {
+        &self.operator
+    }
+
+    pub fn get_right(&self) -> &Box<Expr> {
+        &self.right
+    }
+
+}
+
+//> expr-call
+#[derive(Clone)]
+pub struct Call {
+    callee: Box<Expr>,
+    paren: Token,
+    arguments: Vec<Expr>,
+}
+
+impl Call {
+    pub fn new(callee: Expr, 
+        paren: Token, 
+        arguments: Vec<Expr>
+    ) -> Self {
+        Call {
+            callee: Box::new(callee),
+            paren,
+            arguments,
+        }
+    }
+
+    pub fn get_callee(&self) -> &Box<Expr> {
+        &self.callee
+    }
+
+    pub fn get_paren(&self) -> &Token {
+        &self.paren
+    }
+
+    pub fn get_arguments(&self) -> &Vec<Expr> {
+        &self.arguments
+    }
+
+}
+
+//> expr-get
+#[derive(Clone)]
+pub struct Get {
+    object: Box<Expr>,
+    name: Token,
+}
+
+impl Get {
+    pub fn new(object: Expr, 
+        name: Token
+    ) -> Self {
+        Get {
+            object: Box::new(object),
+            name,
+        }
+    }
+
+    pub fn get_object(&self) -> &Box<Expr> {
+        &self.object
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+}
+
+//> expr-grouping
+#[derive(Clone)]
+pub struct Grouping {
+    expression: Box<Expr>,
+}
+
+impl Grouping {
+    pub fn new(expression: Expr) -> Self {
+        Grouping {
+            expression: Box::new(expression),
+        }
+    }
+
+    pub fn get_expression(&self) -> &Box<Expr> {
+        &self.expression
+    }
+
+}
+
+//> expr-literal
+#[derive(Clone)]
+pub struct Literal {
+    value: LiteralValue,
+}
+
+impl Literal {
+    pub fn new(value: LiteralValue) -> Self {
+        Literal {
+            value,
+        }
+    }
+
+    pub fn get_value(&self) -> &LiteralValue {
+        &self.value
+    }
+
+}
+
+//> expr-logical
+#[derive(Clone)]
+pub struct Logical {
+    left: Box<Expr>,
+    operator: Token,
+    right: Box<Expr>,
+}
+
+impl Logical {
+    pub fn new(left: Expr, 
+        operator: Token, 
+        right: Expr
+    ) -> Self {
+        Logical {
+            left: Box::new(left),
+            operator,
+            right: Box::new(right),
+        }
+    }
+
+    pub fn get_left(&self) -> &Box<Expr> {
+        &self.left
+    }
+
+    pub fn get_operator(&self) -> &Token {
+        &self.operator
+    }
+
+    pub fn get_right(&self) -> &Box<Expr> {
+        &self.right
+    }
+
+}
+
+//> expr-set
+#[derive(Clone)]
+pub struct Set {
+    object: Box<Expr>,
+    name: Token,
+    value: Box<Expr>,
+}
+
+impl Set {
+    pub fn new(object: Expr, 
+        name: Token, 
+        value: Expr
+    ) -> Self {
+        Set {
+            object: Box::new(object),
+            name,
+            value: Box::new(value),
+        }
+    }
+
+    pub fn get_object(&self) -> &Box<Expr> {
+        &self.object
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+    pub fn get_value(&self) -> &Box<Expr> {
+        &self.value
+    }
+
+}
+
+//> expr-super
+#[derive(Clone)]
+pub struct Super {
+    keyword: Token,
+    method: Token,
+}
+
+impl Super {
+    pub fn new(keyword: Token, 
+        method: Token
+    ) -> Self {
+        Super {
+            keyword,
+            method,
+        }
+    }
+
+    pub fn get_keyword(&self) -> &Token {
+        &self.keyword
+    }
+
+    pub fn get_method(&self) -> &Token {
+        &self.method
+    }
+
+}
+
+//> expr-this
+#[derive(Clone)]
+pub struct This {
+    keyword: Token,
+}
+
+impl This {
+    pub fn new(keyword: Token) -> Self {
+        This {
+            keyword,
+        }
+    }
+
+    pub fn get_keyword(&self) -> &Token {
+        &self.keyword
+    }
+
+}
+
+//> expr-unary
+#[derive(Clone)]
+pub struct Unary {
+    operator: Token,
+    right: Box<Expr>,
+}
+
+impl Unary {
+    pub fn new(operator: Token, 
+        right: Expr
+    ) -> Self {
+        Unary {
+            operator,
+            right: Box::new(right),
+        }
+    }
+
+    pub fn get_operator(&self) -> &Token {
+        &self.operator
+    }
+
+    pub fn get_right(&self) -> &Box<Expr> {
+        &self.right
+    }
+
+}
+
+//> expr-variable
+#[derive(Clone)]
+pub struct Variable {
+    name: Token,
+}
+
+impl Variable {
+    pub fn new(name: Token) -> Self {
+        Variable {
+            name,
+        }
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+}
+
+// Define enum
+#[derive(Clone)]
+pub enum Expr {
+    Assign(Assign),
+    Binary(Binary),
+    Call(Call),
+    Get(Get),
+    Grouping(Grouping),
+    Literal(Literal),
+    Logical(Logical),
+    Set(Set),
+    Super(Super),
+    This(This),
+    Unary(Unary),
+    Variable(Variable),
+}
+
+// Visitor Trait
+pub trait Visitor<T> {
+    fn visit_assign_expr(&self, expr: &Assign) -> T;
+    fn visit_binary_expr(&self, expr: &Binary) -> T;
+    fn visit_call_expr(&self, expr: &Call) -> T;
+    fn visit_get_expr(&self, expr: &Get) -> T;
+    fn visit_grouping_expr(&self, expr: &Grouping) -> T;
+    fn visit_literal_expr(&self, expr: &Literal) -> T;
+    fn visit_logical_expr(&self, expr: &Logical) -> T;
+    fn visit_set_expr(&self, expr: &Set) -> T;
+    fn visit_super_expr(&self, expr: &Super) -> T;
+    fn visit_this_expr(&self, expr: &This) -> T;
+    fn visit_unary_expr(&self, expr: &Unary) -> T;
+    fn visit_variable_expr(&self, expr: &Variable) -> T;
+}
+
+// Implement `accept()` for `Expr`
+impl Expr {
+    pub fn accept<T>(&self, visitor: &dyn Visitor<T>) -> T {
+        match self {
+            Expr::Assign(val) => visitor.visit_assign_expr(val),
+            Expr::Binary(val) => visitor.visit_binary_expr(val),
+            Expr::Call(val) => visitor.visit_call_expr(val),
+            Expr::Get(val) => visitor.visit_get_expr(val),
+            Expr::Grouping(val) => visitor.visit_grouping_expr(val),
+            Expr::Literal(val) => visitor.visit_literal_expr(val),
+            Expr::Logical(val) => visitor.visit_logical_expr(val),
+            Expr::Set(val) => visitor.visit_set_expr(val),
+            Expr::Super(val) => visitor.visit_super_expr(val),
+            Expr::This(val) => visitor.visit_this_expr(val),
+            Expr::Unary(val) => visitor.visit_unary_expr(val),
+            Expr::Variable(val) => visitor.visit_variable_expr(val),
+        }
+    }
+}
+//< Appendix II expr
---
+++ src/stmt.rs
@@ -0,0 +1,288 @@
+//> Appendix II stmt
+use super::token::Token;
+use super::expr::Expr;
+
+//> stmt-block
+#[derive(Clone)]
+pub struct Block {
+    statements: Vec<Stmt>,
+}
+
+impl Block {
+    pub fn new(statements: Vec<Stmt>) -> Self {
+        Block {
+            statements,
+        }
+    }
+
+    pub fn get_statements(&self) -> &Vec<Stmt> {
+        &self.statements
+    }
+
+}
+
+//> stmt-class
+#[derive(Clone)]
+pub struct Class {
+    name: Token,
+    superclass: Option<Expr>,
+    methods: Vec<Function>,
+}
+
+impl Class {
+    pub fn new(name: Token, 
+        superclass: Option<Expr>, 
+        methods: Vec<Function>
+    ) -> Self {
+        Class {
+            name,
+            superclass,
+            methods,
+        }
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+    pub fn get_superclass(&self) -> &Option<Expr> {
+        &self.superclass
+    }
+
+    pub fn get_methods(&self) -> &Vec<Function> {
+        &self.methods
+    }
+
+}
+
+//> stmt-expression
+#[derive(Clone)]
+pub struct Expression {
+    expression: Expr,
+}
+
+impl Expression {
+    pub fn new(expression: Expr) -> Self {
+        Expression {
+            expression,
+        }
+    }
+
+    pub fn get_expression(&self) -> &Expr {
+        &self.expression
+    }
+
+}
+
+//> stmt-function
+#[derive(Clone)]
+pub struct Function {
+    name: Token,
+    params: Vec<Token>,
+    body: Vec<Stmt>,
+}
+
+impl Function {
+    pub fn new(name: Token, 
+        params: Vec<Token>, 
+        body: Vec<Stmt>
+    ) -> Self {
+        Function {
+            name,
+            params,
+            body,
+        }
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+    pub fn get_params(&self) -> &Vec<Token> {
+        &self.params
+    }
+
+    pub fn get_body(&self) -> &Vec<Stmt> {
+        &self.body
+    }
+
+}
+
+//> stmt-if
+#[derive(Clone)]
+pub struct If {
+    condition: Expr,
+    then_branch: Option<Box<Stmt>>,
+    else_branch: Option<Box<Stmt>>,
+}
+
+impl If {
+    pub fn new(condition: Expr, 
+        then_branch: Stmt, 
+        else_branch: Stmt
+    ) -> Self {
+        If {
+            condition,
+            then_branch: Some(Box::new(then_branch)),
+            else_branch: Some(Box::new(else_branch)),
+        }
+    }
+
+    pub fn get_condition(&self) -> &Expr {
+        &self.condition
+    }
+
+    pub fn get_then_branch(&self) -> &Option<Box<Stmt>> {
+        &self.then_branch
+    }
+
+    pub fn get_else_branch(&self) -> &Option<Box<Stmt>> {
+        &self.else_branch
+    }
+
+}
+
+//> stmt-print
+#[derive(Clone)]
+pub struct Print {
+    expression: Expr,
+}
+
+impl Print {
+    pub fn new(expression: Expr) -> Self {
+        Print {
+            expression,
+        }
+    }
+
+    pub fn get_expression(&self) -> &Expr {
+        &self.expression
+    }
+
+}
+
+//> stmt-return
+#[derive(Clone)]
+pub struct Return {
+    keyword: Token,
+    value: Option<Expr>,
+}
+
+impl Return {
+    pub fn new(keyword: Token, 
+        value: Option<Expr>
+    ) -> Self {
+        Return {
+            keyword,
+            value,
+        }
+    }
+
+    pub fn get_keyword(&self) -> &Token {
+        &self.keyword
+    }
+
+    pub fn get_value(&self) -> &Option<Expr> {
+        &self.value
+    }
+
+}
+
+//> stmt-var
+#[derive(Clone)]
+pub struct Var {
+    name: Token,
+    initializer: Option<Expr>,
+}
+
+impl Var {
+    pub fn new(name: Token, 
+        initializer: Option<Expr>
+    ) -> Self {
+        Var {
+            name,
+            initializer,
+        }
+    }
+
+    pub fn get_name(&self) -> &Token {
+        &self.name
+    }
+
+    pub fn get_initializer(&self) -> &Option<Expr> {
+        &self.initializer
+    }
+
+}
+
+//> stmt-while
+#[derive(Clone)]
+pub struct While {
+    condition: Expr,
+    body: Box<Stmt>,
+}
+
+impl While {
+    pub fn new(condition: Expr, 
+        body: Stmt
+    ) -> Self {
+        While {
+            condition,
+            body: Box::new(body),
+        }
+    }
+
+    pub fn get_condition(&self) -> &Expr {
+        &self.condition
+    }
+
+    pub fn get_body(&self) -> &Box<Stmt> {
+        &self.body
+    }
+
+}
+
+// Define enum
+#[derive(Clone)]
+pub enum Stmt {
+    Block(Block),
+    Class(Class),
+    Expression(Expression),
+    Function(Function),
+    If(If),
+    Print(Print),
+    Return(Return),
+    Var(Var),
+    While(While),
+}
+
+// Visitor Trait
+pub trait Visitor<T> {
+    fn visit_block_stmt(&self, stmt: &Block) -> T;
+    fn visit_class_stmt(&self, stmt: &Class) -> T;
+    fn visit_expression_stmt(&self, stmt: &Expression) -> T;
+    fn visit_function_stmt(&self, stmt: &Function) -> T;
+    fn visit_if_stmt(&self, stmt: &If) -> T;
+    fn visit_print_stmt(&self, stmt: &Print) -> T;
+    fn visit_return_stmt(&self, stmt: &Return) -> T;
+    fn visit_var_stmt(&self, stmt: &Var) -> T;
+    fn visit_while_stmt(&self, stmt: &While) -> T;
+}
+
+// Implement `accept()` for `Stmt`
+impl Stmt {
+    pub fn accept<T>(&self, visitor: &dyn Visitor<T>) -> T {
+        match self {
+            Stmt::Block(val) => visitor.visit_block_stmt(val),
+            Stmt::Class(val) => visitor.visit_class_stmt(val),
+            Stmt::Expression(val) => visitor.visit_expression_stmt(val),
+            Stmt::Function(val) => visitor.visit_function_stmt(val),
+            Stmt::If(val) => visitor.visit_if_stmt(val),
+            Stmt::Print(val) => visitor.visit_print_stmt(val),
+            Stmt::Return(val) => visitor.visit_return_stmt(val),
+            Stmt::Var(val) => visitor.visit_var_stmt(val),
+            Stmt::While(val) => visitor.visit_while_stmt(val),
+        }
+    }
+}
+//< Appendix II stmt
