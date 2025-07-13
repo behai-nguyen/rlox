c89a27e07f4c5dc2473311d8502a43668f89f348
behai-nguyen <behai_nguyen@hotmail.com>
2025-07-14 00:12:49 +1000

    Parsing and Evaluating Expressions
    Added assert_scan_script(), assert_parse_script(), and assert_parse_line()

--- tests/test_common.rs
+++ tests/test_common.rs
@@ -2,12 +2,64 @@
 
 use std::fs::read_to_string;
 
-use rlox::token::LiteralValue;
+use rlox::token::{LiteralValue, Token};
+use rlox::scanner::Scanner;
+use rlox::expr::Expr;
+use rlox::parser::Parser;
 
 pub fn get_script_contents(scriptfile: &str) -> Result<String, std::io::Error> {
     let contents = read_to_string(scriptfile)?;
 
     Ok(contents)
+}
+
+// Load script and scan it.
+pub fn assert_scan_script(scriptfile: &str) -> Vec<Token> {
+    let res = read_to_string(scriptfile);
+    // Check that the script file was loaded successfully.
+    assert!(res.is_ok(), "Failed to load or scan script: {}", scriptfile);
+
+    // Attempt to scan the loaded script file.
+    let res = Scanner::new(&res.unwrap()).scan_tokens();
+    // Check that the script file was scanned successfully.
+    assert_eq!(false, res.is_err());
+
+    res.unwrap()
+}
+
+// Load script and parse it.
+pub fn assert_parse_script(scriptfile: &str) -> Expr {
+    // Ensure scripted loaded and scanned successfully.
+    let tokens = assert_scan_script(scriptfile);
+
+    // Attempt to parse.
+    let parser = Parser::new(&tokens);
+    let res = parser.parse();
+
+    // Ensure parsing is successful.
+    assert_eq!(res.is_err(), false);
+    
+    res.unwrap()
+}
+
+// Parse a single line script.
+pub fn assert_parse_line(line: &str) -> Expr {
+    // Attempt to scan the script line.
+    let res = Scanner::new(line).scan_tokens();
+
+    // Check that the script line was scanned successfully.
+    assert!(res.is_ok(), "Failed to scan line: {}", line);
+
+    let tokens = res.unwrap();
+
+    // Attempt to parse.
+    let parser = Parser::new(&tokens);
+    let res = parser.parse();
+
+    // Ensure parsing is successful.
+    assert_eq!(res.is_err(), false);
+    
+    res.unwrap()
 }
 
 pub fn assert_literal_number(val: &Option<LiteralValue>, number: f64) {
