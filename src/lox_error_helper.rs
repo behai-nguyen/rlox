54d8fd6842de7f8b0a33134eb95131f8c727e0cc
behai-nguyen <behai_nguyen@hotmail.com>
2025-07-22 14:29:25 +1000

    Statements and State
    Implement Stmt::Expression, Stmt::Print, Stmt::Var, Expr::Variable, Expr::Assign and Stmt::Block.
    Application wide error helper functions:
    - Fixing error reporting: mirror the author's report.

---
+++ src/lox_error_helper.rs
@@ -0,0 +1,18 @@
+/* Date Created: 16/07/2025. */
+
+//! Rust-specific. Helper function to create instances of 
+//! [`super::lox_error::LoxError`].
+
+use super::lox_error::LoxError;
+use super::token::Token;
+
+/// Report an error at a line and a character. Used by the scanner.
+pub fn scanner_error(line: usize, c: char, message: &str) -> LoxError {
+    LoxError::new(line, &c.to_string(), &format!("{}", message))
+}
+
+/// Global error report function. All code should call this 
+/// to create return error for `Result<T, LoxError>`.
+pub fn error(token: &Token, message: &str) -> LoxError {
+    LoxError::new(token.get_line(), token.get_lexeme(), &format!("{}", message))
+}
