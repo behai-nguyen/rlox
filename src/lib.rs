56323275e816ebc609c76e236d7773a73d80b880
behai-nguyen <behai_nguyen@hotmail.com>
2025-07-22 14:30:42 +1000

    Statements and State
    Implement Stmt::Expression, Stmt::Print, Stmt::Var, Expr::Variable, Expr::Assign and Stmt::Block.
    Added data_type, lox_error_helper.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,15 @@
 // as "use rlox::scanner" is possible.
 
 pub mod lox_error;
+pub mod lox_error_helper;
 pub mod scanner_index;
 pub mod token_type;
 pub mod token;
+pub mod data_type;
 pub mod scanner;
 pub mod expr;
 pub mod stmt;
 pub mod parser;
 pub mod ast_printer;
+pub mod environment;
 pub mod interpreter;
