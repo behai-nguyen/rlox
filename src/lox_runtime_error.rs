cab61260cb1ff3c60348a1d385f6cae3ff5a3c47
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-01 21:52:27 +1000

    Functions: native and declared functions, return and closure
    Implement Expr::Call, Stmt::Function and Stmt::Return
    - Encapsulates function return value via 'Result' and early returns

---
+++ src/lox_runtime_error.rs
@@ -0,0 +1,61 @@
+/* Date Created: 28/07/2025. */
+
+//! Chapter 10: Functions, the 
+//! [Return Statements](https://craftinginterpreters.com/functions.html#return-statements)
+//! section.
+//! 
+
+// Rust-specific.
+
+use std::fmt;
+
+use super::lox_error::LoxError;
+use super::lox_return::LoxReturn;
+
+// `Return.java` creates a custom unchecked exception to signal early exits 
+// from Lox functions. In Rust, we use **control flow via `Result` and early 
+// returns**.
+// 
+// It is used in ./src/lox_function.rs where interpreter.execute_block() 
+// returns Result<(), LoxRuntimeError>:
+//
+//    return match interpreter.execute_block(&self.declaration.get_body(), environment) {
+//        Err(LoxRuntimeError::Return(ret)) => {
+//            Ok(ret.value)
+//        }
+//        Err(LoxRuntimeError::Error(err)) => Err(LoxRuntimeError::Error(err)),
+//        Ok(_) => {
+//            Ok(Value::Nil)
+//        }
+//    };
+//
+#[derive(Debug)]
+pub enum LoxRuntimeError {
+    Error(LoxError),
+    Return(LoxReturn),
+}
+
+impl From<LoxError> for LoxRuntimeError {
+    fn from(error: LoxError) -> Self {
+        LoxRuntimeError::Error(error)
+    }
+}
+
+impl From<LoxRuntimeError> for LoxError {
+    fn from(error: LoxRuntimeError) -> Self {
+        let inner = match error {
+            LoxRuntimeError::Error(e) => e,
+            _ => unreachable!("Expected RuntimeError::Error"),
+        };
+        inner
+    }
+}
+
+impl fmt::Display for LoxRuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LoxRuntimeError::Error(err) => write!(f, "Error: {}", err),
+            LoxRuntimeError::Return(ret) => write!(f, "Return: {}", ret),
+        }
+    }
+}
