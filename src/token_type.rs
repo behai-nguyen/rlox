ae07b7c4cb4628c60d0003cb585e57c32baefee6
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-12 14:13:38 +1000

    Chapter 11: Resolving and Binding
    No new Expr/Stmt implemented
    Required Hash and Eq traits on enum:
    - pub enum TokenType derived #[derive(Debug, Clone, Hash, Eq, PartialEq)]
    Rewrote the Display trait.

--- src/token_type.rs
+++ src/token_type.rs
@@ -5,7 +5,7 @@
 
 use std::fmt;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub enum TokenType {
     // Single-character tokens.
     LeftParen, 
@@ -56,51 +56,7 @@ pub enum TokenType {
 }
 
 impl fmt::Display for TokenType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            // Single-character tokens.
-            TokenType::LeftParen => write!(f, "LeftParen"),
-            TokenType::RightParen => write!(f, "RightParen"),
-            TokenType::LeftBrace => write!(f, "LeftBrace"),
-            TokenType::RightBrace => write!(f, "RightBrace"),
-            TokenType::Comma => write!(f, "Comma"),
-            TokenType::Dot => write!(f, "Dot"),
-            TokenType::Minus => write!(f, "Minus"),
-            TokenType::Plus => write!(f, "Plus"),
-            TokenType::Semicolon => write!(f, "Semicolon"),
-            TokenType::Slash => write!(f, "Slash"),
-            TokenType::Star => write!(f, "Star"),
-            // One or two character tokens.
-            TokenType::Bang => write!(f, "Bang"),
-            TokenType::BangEqual => write!(f, "BangEqual"),
-            TokenType::Equal => write!(f, "Equal"),
-            TokenType::EqualEqual => write!(f, "EqualEqual"),
-            TokenType::Greater => write!(f, "Greater"),
-            TokenType::GreaterEqual => write!(f, "GreaterEqual"),
-            TokenType::Less => write!(f, "Less"),
-            TokenType::LessEqual => write!(f, "LessEqual"),
-            // Literals.
-            TokenType::Identifier => write!(f, "Identifier"),
-            TokenType::String => write!(f, "String"),
-            TokenType::Number => write!(f, "Number"),
-            // Keywords.
-            TokenType::And => write!(f, "And"),
-            TokenType::Class => write!(f, "Class"),
-            TokenType::Else => write!(f, "Else"),
-            TokenType::False => write!(f, "False"),
-            TokenType::Fun => write!(f, "Fun"),
-            TokenType::For => write!(f, "For"),
-            TokenType::If => write!(f, "If"),
-            TokenType::Nil => write!(f, "Nil"),
-            TokenType::Or => write!(f, "Or"),
-            TokenType::Print => write!(f, "Print"),
-            TokenType::Return => write!(f, "Return"),
-            TokenType::Super => write!(f, "Super"),
-            TokenType::This => write!(f, "This"),
-            TokenType::True => write!(f, "True"),
-            TokenType::Var => write!(f, "Var"),
-            TokenType::While => write!(f, "While"),
-            TokenType::Eof => write!(f, "Eof"),
-        }
-    }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "{:?}", self)
+  }
 }
