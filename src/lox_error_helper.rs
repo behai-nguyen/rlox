0a2ac7a1a0ec5b90ce16a86ca77d03518b05d751
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-20 01:07:09 +1000

    Classes: declarations, instances, properties, methods, this, constructors and initialisers
    Implement Stmt::Class, Expr::Get, Expr::Set, Expr::This
    Added pub fn sys_error(lexeme: &str, message: &str) -> LoxError
    Updated scanner_error() and error:
    - using param `message` directly instead of via format!

--- src/lox_error_helper.rs
+++ src/lox_error_helper.rs
@@ -8,11 +8,17 @@ use super::token::Token;
 
 /// Report an error at a line and a character. Used by the scanner.
 pub fn scanner_error(line: usize, c: char, message: &str) -> LoxError {
-    LoxError::new(line, &c.to_string(), &format!("{}", message))
+    LoxError::new(line, &c.to_string(), message)
 }
 
-/// Global error report function. All code should call this 
-/// to create return error for `Result<T, LoxError>`.
+/// Global error report function. All code using Token should 
+/// call this to create return error for `Result<T, LoxError>`.
 pub fn error(token: &Token, message: &str) -> LoxError {
-    LoxError::new(token.line(), token.lexeme(), &format!("{}", message))
+    LoxError::new(token.line(), token.lexeme(), message)
+}
+
+/// Global error report function. All code which do not use Token 
+/// should call this to create return error for `Result<T, LoxError>`.
+pub fn sys_error(lexeme: &str, message: &str) -> LoxError {
+    LoxError::new(0, lexeme, message)
 }
