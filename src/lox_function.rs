50352ceadad1a3fab466ef9251942d3d25c6a0d9
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-20 01:05:20 +1000

    Classes: declarations, instances, properties, methods, this, constructors and initialisers
    Implement Stmt::Class, Expr::Get, Expr::Set, Expr::This
    Now implements `impl std::fmt::Debug`, `PartialEq`, `Clone` and fmt::Display manually.
    Removed to_string() from implementation
    Added `pub fn bind(&self, instance: Rc<RefCell<LoxInstance>>) -> LoxFunction`
    `data_type.rs` renamed to `value.rs`
    Other updates as per Chapter 12.

--- src/lox_function.rs
+++ src/lox_function.rs
@@ -10,22 +10,61 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 use crate::stmt;
+use super::lox_runtime_error::LoxRuntimeError;
 use super::interpreter::Interpreter;
-use super::data_type::Value;
+use super::value::Value;
 use super::environment::{Environment, EnvironmentRef};
 use super::lox_callable::LoxCallable;
+use super::lox_instance::LoxInstance;
 
-use super::lox_runtime_error::LoxRuntimeError;
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct LoxFunction {
     declaration: stmt::Function,
     closure: EnvironmentRef,
+    is_initializer: bool,
 }
 
 impl LoxFunction {
-    pub fn new(declaration: stmt::Function, closure: EnvironmentRef) -> Self {
-        Self { declaration, closure }
+    pub fn new(declaration: stmt::Function, closure: EnvironmentRef, is_initializer: bool) -> Self {
+        Self { declaration, closure, is_initializer }
+    }
+
+    pub fn bind(&self, instance: Rc<RefCell<LoxInstance>>) -> LoxFunction {
+        let mut env = Environment::new_local_scope(self.closure.clone());
+        env.define("this".to_string(), Value::LoxInstance(instance));
+        LoxFunction::new(self.declaration.clone(), 
+            Rc::new(RefCell::new(env)), self.is_initializer)
+    }
+}
+
+impl std::fmt::Debug for LoxFunction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "LoxFunction {{ name: {} }}", self.declaration.name().lexeme())
+    }
+}
+
+// We need to take charge of implementing `PartialEq` and `Clone` for 
+// `LoxFunction`, due to:
+//
+// `closure: EnvironmentRef` is an `Rc<RefCell<Environment>>`, and `Environment` 
+// contains a `HashMap<String, Value>`, which may include functions, which 
+// contain environments...
+//
+// So `PartialEq` on `LoxFunction` can recurse infinitely or deeply through cyclic 
+// structures: this would eventually result in stack overflow.
+impl PartialEq for LoxFunction {
+    fn eq(&self, other: &Self) -> bool {
+        Rc::ptr_eq(&self.closure, &other.closure)
+            && self.declaration == other.declaration
+    }
+}
+
+impl Clone for LoxFunction {
+    fn clone(&self) -> Self {
+        LoxFunction {
+            declaration: self.declaration.clone(),
+            closure: Rc::clone(&self.closure), // Shallow clone.
+            is_initializer: self.is_initializer,
+        }
     }
 }
 
@@ -46,11 +85,19 @@ impl LoxCallable for LoxFunction {
 
         return match interpreter.execute_block(&self.declaration.body(), environment) {
             Err(LoxRuntimeError::Return(ret)) => {
-                Ok(ret.value)
+                if self.is_initializer {
+                    Ok(Environment::get_at(&self.closure, 0, "this"))
+                } else {
+                    Ok(ret.value)
+                }
             }
             Err(LoxRuntimeError::Error(err)) => Err(LoxRuntimeError::Error(err)),
             Ok(_) => {
-                Ok(Value::Nil)
+                if self.is_initializer {
+                    Ok(Environment::get_at(&self.closure, 0, "this"))
+                } else {
+                    Ok(Value::Nil)
+                }
             }
         }
     }
@@ -58,8 +105,10 @@ impl LoxCallable for LoxFunction {
     fn as_any(&self) -> &dyn Any {
         self
     }
+}
 
-    fn to_string(&self) -> String {
-        format!("<fn {}>", self.declaration.name().lexeme())
+impl std::fmt::Display for LoxFunction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "<fn {}>", self.declaration.name().lexeme())
     }
 }
