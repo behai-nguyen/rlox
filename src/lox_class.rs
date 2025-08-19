8e7f6bebf09d48bcbed55360efb8016921ea1cbd
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-20 01:00:24 +1000

    Classes: declarations, instances, properties, methods, this, constructors and initialisers
    Implement Stmt::Class, Expr::Get, Expr::Set, Expr::This

---
+++ src/lox_class.rs
@@ -0,0 +1,72 @@
+/* Date Created: 12/08/2025. */
+
+//! The **Class Declarations** section in  
+//! [https://craftinginterpreters.com/classes.html](https://craftinginterpreters.com/classes.html). 
+//! 
+//! The runtime representation of a class.
+//! 
+
+use std::any::Any;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+use super::lox_runtime_error::LoxRuntimeError;
+use super::interpreter::Interpreter;
+use super::value::Value;
+use super::lox_callable::LoxCallable;
+use super::lox_instance::LoxInstance;
+use super::lox_function::LoxFunction;
+
+pub type LoxFunctionsMap = HashMap<String, LoxFunction>;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LoxClass {
+    name: String,
+    methods: LoxFunctionsMap,
+}
+
+impl LoxClass {
+    pub fn new(name: String, methods: LoxFunctionsMap) -> Self {
+        LoxClass { 
+            name, 
+            methods,
+        }
+    }
+
+    pub fn find_method(&self, name: &str) -> Option<&LoxFunction> {
+        self.methods.get(name)
+    }
+}
+
+impl std::fmt::Display for LoxClass {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl LoxCallable for LoxClass {
+    fn arity(&self) -> usize {
+        if let Some(initializer) = self.find_method("init") {
+            initializer.arity()
+        } else {
+            0
+        }
+    }
+
+    fn call(&self, interpreter: &mut Interpreter, arguments: Vec<Value>) -> Result<Value, LoxRuntimeError> {
+        let instance = LoxInstance::new(self.clone());
+        let instance_ref = Rc::new(RefCell::new(instance));
+
+        if let Some(initializer) = self.find_method("init") {
+            initializer.bind(
+                Rc::clone(&instance_ref))
+                .call(interpreter, arguments)?;
+        }
+        Ok(Value::LoxInstance(instance_ref))
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
