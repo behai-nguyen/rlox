1cd08722c24081c8d29d9b22ce591ecaa8261039
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-20 01:02:07 +1000

    Classes: declarations, instances, properties, methods, this, constructors and initialisers
    Implement Stmt::Class, Expr::Get, Expr::Set, Expr::This
    Module `data_type.rs` renamed to `value.rs`
    Enum type name is now `Value`
    Added LoxInstance(Rc<RefCell<LoxInstance>>) variant
    Added custom debug `impl std::fmt::Debug for Value`
    LoxCallable refactored to use Rc: `LoxCallable(Rc<dyn LoxCallable>)`
    - reinstated `#[derive(Clone)]` for `Value`
    - removed `impl Clone for Value`
    In `impl PartialEq for Value`: bug fixed LoxCallable using the raw pointer addresses
    Added `pub type ValueMap = HashMap<String, Value>;`
    Added `impl fmt::Display for Value`
    Added a unit test section.

--- src/value.rs
+++ src/value.rs
@@ -3,48 +3,90 @@
 //! Lox supported data types. This is Rust-specific, this module is not in the 
 //! original Java version.
 
-use super::lox_callable::LoxCallable;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fmt;
 
+use super::lox_callable::LoxCallable;
+use super::lox_instance::LoxInstance;
 
 // Rust-specific.
-// Rust doesn’t support deriving Clone, PartialEq automatically for trait objects, 
-// which Box<dyn LoxCallable> is one.
-#[derive(Debug)]
-pub enum DataType {
+
+// To run test for this module only: 
+// 
+//     * cargo test value::tests
+//
+//     * cargo test value::tests::value_comparison_and_clone -- --exact [--nocapture]
+//
+
+
+// Rust doesn’t support deriving PartialEq automatically for trait objects, 
+// which Rc<dyn LoxCallable> is one.
+#[derive(Clone)]
+pub enum Value {
     Number(f64),
     String(String),
     Boolean(bool),
     Nil,
-    LoxCallable(Box<dyn LoxCallable>),
+    LoxCallable(Rc<dyn LoxCallable>),
+    LoxInstance(Rc<RefCell<LoxInstance>>),
 }
 
-pub type Value = DataType;
+pub type ValueMap = HashMap<String, Value>;
 
-// Box<dyn LoxCallable> require manual implementations because trait objects don’t 
-// support Clone or PartialEq by default.
-impl Clone for DataType {
-    fn clone(&self) -> Self {
+impl std::fmt::Debug for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            DataType::Number(n) => DataType::Number(*n),
-            DataType::String(s) => DataType::String(s.clone()),
-            DataType::Boolean(b) => DataType::Boolean(*b),
-            DataType::Nil => DataType::Nil,
-            DataType::LoxCallable(callable) => DataType::LoxCallable(callable.clone_box()),
+            Value::Number(n) => write!(f, "Number({})", n),
+            Value::String(s) => write!(f, "String({})", s),
+            Value::Boolean(b) => write!(f, "Boolean({})", b),
+            Value::Nil => write!(f, "Nil"),
+            Value::LoxInstance(_) => write!(f, "LoxInstance"),
+            Value::LoxCallable(_) => write!(f, "LoxCallable"),
         }
     }
 }
 
-// Box<dyn LoxCallable> require manual implementations because trait objects don’t 
-// support Clone or PartialEq by default.
-impl PartialEq for DataType {
+// Rc<dyn LoxCallable> require manual implementations because trait objects don’t 
+// support PartialEq by default.
+impl PartialEq for Value {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (DataType::Number(a), DataType::Number(b)) => a == b,
-            (DataType::String(a), DataType::String(b)) => a == b,
-            (DataType::Boolean(a), DataType::Boolean(b)) => a == b,
-            (DataType::Nil, DataType::Nil) => true,
-            (DataType::LoxCallable(a), DataType::LoxCallable(b)) => a == b,
+            (Value::Number(a), Value::Number(b)) => a == b,
+            (Value::String(a), Value::String(b)) => a == b,
+            (Value::Boolean(a), Value::Boolean(b)) => a == b,
+            (Value::Nil, Value::Nil) => true,
+            // Compare the raw pointer addresses.
+            (Value::LoxCallable(a), Value::LoxCallable(b)) =>
+                std::ptr::eq(a.as_ref(), b.as_ref()),
+            (Value::LoxInstance(i1), Value::LoxInstance(i2)) => i1 == i2,
             _ => false,
         }
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Number(n) => write!(f, "{}", n),
+            Value::String(s) => write!(f, "{}", s),
+            Value::Boolean(b) => write!(f, "{}", b),
+            Value::Nil => write!(f, "nil"),
+            Value::LoxCallable(callable) => write!(f, "{}", callable),
+            Value::LoxInstance(instance) => write!(f, "{}", instance.borrow().to_string()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_comparison_and_clone() {
+        let a = Value::Number(42.0);
+        let b = a.clone();
+        assert_eq!(a, b);
     }
 }
