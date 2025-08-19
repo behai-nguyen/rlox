5afee9f10e2db24966312d8c46aa3fd0e55ba5ac
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-20 01:05:39 +1000

    Classes: declarations, instances, properties, methods, this, constructors and initialisers
    Implement Stmt::Class, Expr::Get, Expr::Set, Expr::This
    In data_type.rs, LoxCallable refactored to use Rc, `LoxCallable(Rc<dyn LoxCallable>)`:
    - removed `pub trait CloneLoxCallable`
    - removed `impl<T> CloneLoxCallable for T`
    - LoxCallable is now `pub trait LoxCallable: Debug + fmt::Display`
    `data_type.rs` renamed to `value.rs`
    Removed `fn to_string(&self) -> String;` from `pub trait LoxCallable`
    Removed `impl PartialEq for Box<dyn LoxCallable>`:
    - removed pub trait PartialEqLoxCallable
    - impl<T> PartialEqLoxCallable for T

--- src/lox_callable.rs
+++ src/lox_callable.rs
@@ -5,55 +5,18 @@
 //! section, [Interpreting function calls](https://craftinginterpreters.com/functions.html#interpreting-function-calls) 
 //! subsection.
 
-use std::fmt::Debug;
+use std::fmt;
 use std::any::Any;
 
 use super::interpreter::Interpreter;
 use super::lox_runtime_error::LoxRuntimeError;
-use super::data_type::Value;
+use super::value::Value;
 
-// Box<dyn LoxCallable> require manual implementations because trait objects don’t 
-// support Clone or PartialEq by default.
-pub trait CloneLoxCallable {
-    fn clone_box(&self) -> Box<dyn LoxCallable>;
-}
-
-impl<T> CloneLoxCallable for T
-where
-    T: LoxCallable + Clone + 'static,
-{
-    fn clone_box(&self) -> Box<dyn LoxCallable> {
-        Box::new(self.clone())
-    }
-}
-
-// Box<dyn LoxCallable> require manual implementations because trait objects don’t 
-// support Clone or PartialEq by default.
-pub trait PartialEqLoxCallable {
-    fn equals_callable(&self, other: &dyn LoxCallable) -> bool;
-}
-
-impl<T> PartialEqLoxCallable for T
-where
-    T: LoxCallable + PartialEq + Any,
-{
-    fn equals_callable(&self, other: &dyn LoxCallable) -> bool {
-        other
-            .as_any()
-            .downcast_ref::<T>()
-            .map_or(false, |other_t| self == other_t)
-    }
-}
-
-pub trait LoxCallable: Debug + CloneLoxCallable + PartialEqLoxCallable {
+pub trait LoxCallable: fmt::Debug + fmt::Display {
     fn arity(&self) -> usize;
     fn call(&self, interpreter: &mut Interpreter, arguments: Vec<Value>) -> Result<Value, LoxRuntimeError>;
+    #[allow(dead_code)]
+    // No downcast_ref::<T>() anywhere in the code yet.
+    // Might remove later, but not now.
     fn as_any(&self) -> &dyn Any;
-    fn to_string(&self) -> String;
-}
-
-impl PartialEq for Box<dyn LoxCallable> {
-    fn eq(&self, other: &Self) -> bool {
-        (**self).equals_callable(&**other)
-    }
 }
