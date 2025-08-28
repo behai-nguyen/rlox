09a445ac3f5ef34b2459e97a2c2cacf34b1624ef
behai-nguyen <behai_nguyen@hotmail.com>
2025-08-28 20:07:46 +1000

    General refactoring: tests, error display
    Bug fixed: check input script file exists
    Bug fixed: check if input file actually exists:
    - refactored main() checks input file when specified
    Added helper method `fn print_error(err: LoxError, originator: &str)`
    Refactored method `run()`:
    - call `print_error()` to print errors

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 use std::env;
 use std::process;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, exists};
 use std::io::{self, Write};
 
 mod expr;
@@ -35,14 +35,29 @@ use rlox::{unwrap_expr, unwrap_stmt};
 use scanner::Scanner;
 use interpreter::Interpreter;
 use resolver::Resolver;
+use lox_error::LoxError;
+
+fn print_error(err: LoxError, originator: &str) {
+    println!("{} error:", originator);
+
+    let err_msgs: Vec<String> = err
+        .to_string()
+        .lines()
+        .map(|line| line.to_string())
+        .collect();
+    for err_msg in err_msgs {
+        println!("    {}", err_msg);
+    }
+}
 
 fn run(source: &str) -> Result<(), std::io::Error> {
     let mut scanner = Scanner::new(source);
     match scanner.scan_tokens() {
+        Err(err) => print_error(err, "Scanner"),
         Ok(tokens) => {
             let mut parser = parser::Parser::new(&tokens);
             match parser.parse() {
-                Err(err) => println!("Parsing error: {}", err),
+                Err(err) => print_error(err, "Parser"),
                 Ok(statements) => { 
                     // Both are valid.
                     // let mut interpreter = Interpreter::new(Box::new(io::stdout()));
@@ -50,10 +65,10 @@ fn run(source: &str) -> Result<(), std::io::Error> {
                     let mut resolver: Resolver = Resolver::new(&mut interpreter);
 
                     match resolver.resolve(&statements) {
-                      Err(err) => println!("Resolve error: {}", err),
+                        Err(err) => print_error(err.into(), "Resolver"),
                         Ok(_) => {
                             match interpreter.interpret(&statements) {
-                                Err(err) => println!("Evaluation error: {}", err),
+                                Err(err) => print_error(err.into(), "Interpreter"),
                                 Ok(_) => (),
                             }
                         }
@@ -61,10 +76,6 @@ fn run(source: &str) -> Result<(), std::io::Error> {
                 },
             }
         },
-        Err(err) => {
-            println!("{}", err);
-            process::exit(65);
-        }
     }
 
     Ok(())
@@ -111,6 +122,11 @@ fn main() {
         println!("Usage: {} [script]", &args[0]);
         process::exit(1);
     } else if args.len() == 2 {
+        if !exists(&args[1]).expect("Can not check if input file exists.") {
+            println!("Input file `{}` does not exist!", &args[1]);
+            process::exit(65);
+        }
+
         let _ = run_file(&args[1]);
     } else {
         let _ = run_prompt();
